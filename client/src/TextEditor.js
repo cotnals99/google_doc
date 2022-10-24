580c3e8d9302f6c9c54e9aec3c253c10b4dc529b
cotnals99 <su.chae@icloud.com>
2022-10-24 09:05:10 +0700

    Completed Clone

---
+++ client/src/TextEditor.js
@@ -0,0 +1,120 @@
+// import Quill from "quill";
+// import "quill/dist/quill.snow.css";
+// import { useEffect } from "react";
+
+// export default function TextEditor(){
+//     useEffect(()=>{
+//         new Quill("#container", {theme: "snow"})
+//     }, [])
+//     return <div id="container"></div>
+// }
+
+import Quill from "quill";
+import "quill/dist/quill.snow.css";
+import { useCallback, useEffect, useState } from "react";
+import io from 'socket.io-client'
+import { useParams } from 'react-router'
+
+const SAVE_INTERVAL_MS = 2000
+
+const TOOLBAR_OPTIONS = [
+    [{header: [1, 2, 3, 4, 5, 6, false]}],
+    [{font: []}],
+    [{list: "ordered"}, {list: "bullet"}],
+    ["bold", "italic", "underline"],
+    [{color: []}, {background: []}],
+    [{script: "sub"}, {script: "super"}],
+    [{align: []}],
+    ["image", "blockquote", "code-block"],
+    ["clean"]
+]
+
+
+export default function TextEditor(){
+
+    const [socket, setSocket] = useState()
+    const [quill, setQuill] = useState()
+    const {id: documentId} = useParams()
+
+    useEffect(() => {
+      const s = io("http://localhost:3001")
+      setSocket(s)
+      console.log("Connected Server")
+    
+      return () => {
+        s.disconnect()
+      }
+    }, [])
+
+    useEffect(()=>{
+        if (socket == null || quill == null) return
+
+        socket.once('load-document', document => {
+            quill.setContents(document)
+            quill.enable()
+        })
+
+        socket.emit('get-document', documentId)
+    }, [socket, quill, documentId])
+
+
+    //Delta is just the small changes that is made, not whole documents
+    useEffect(()=>{
+        if(socket == null || quill == null) return
+
+        const handler = (delta, oldDelta, source)=>{
+            if(source !== 'user') return
+            socket.emit("send-changes", delta)
+        }
+
+        quill.on("text-change", handler)
+
+        return() =>{
+            quill.off('text-change', handler)
+        }
+    }, [socket, quill])
+
+
+
+    useEffect(()=>{
+        if(socket == null || quill == null) return
+
+        const handler = (delta)=>{
+            quill.updateContents(delta)
+        }
+
+        socket.on("receive-changes", handler)
+
+        return() =>{
+            socket.off('receive-changes', handler)
+        }
+    }, [socket, quill])
+    
+
+    useEffect(()=>{
+        if(socket == null || quill == null) return
+
+        const interval = setInterval(()=>{
+            socket.emit('save-document', quill.getContents())
+
+            return () => {
+                clearInterval(interval)
+            }
+        }, SAVE_INTERVAL_MS)
+    }, [socket, quill])
+
+
+    const wrapperRef =  useCallback((wrapper)=>{
+
+        if (wrapper == null) return
+        wrapper.innerHtml = ""
+
+        const editor = document.createElement("div")
+        wrapper.append(editor)
+        const q = new Quill(editor, {theme: "snow", modules: {toolbar : TOOLBAR_OPTIONS}})
+        q.disable()
+        q.setText('Loading...')
+        setQuill(q)
+    }, [])
+    return <div className="container" ref={wrapperRef}></div>
+}
